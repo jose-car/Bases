66d65652dd29506e23c42e4fe824d1c1359a1d2f
jose luis carmona duran <josecarmona000@gmail.com>
2021-10-04 14:33:56 -0500

    Bases

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Personaje{
+  nombre: string,
+  poder: number
+}
+
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent {
+ 
+  nuevo:Personaje = {
+    nombre: 'Truncks',
+    poder: 14000
+  }
+ 
+
+  agregar(){ 
+   console.log(this.nuevo);
+  }
+
+}
